e2321d0825817251afd932a5da6f31ef89ac090a
skzd <63005848+skzd@users.noreply.github.com>
2020-04-04 21:43:35 +0800

    skz

--- src/com/java/Hello.java
+++ src/com/java/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("Hello Test");
         System.out.println("skz改动");
         System.out.println("大帅比又回来了");
+        System.out.println("wjl大帅币");
     }
 }
