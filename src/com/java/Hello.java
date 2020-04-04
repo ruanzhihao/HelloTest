8fa1e639d9d5bed16751dabb664e55a479f43abc
Wjl <1284897325@qq.com>
2020-04-04 21:37:26 +0800

    wjl

--- src/com/java/Hello.java
+++ src/com/java/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Test");
         System.out.println("skz改动");
+        System.out.println("大帅比又回来了");
     }
 }
