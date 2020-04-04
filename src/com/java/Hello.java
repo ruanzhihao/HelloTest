fe8585d9d00a282213d7872c38b536b903ac0d60
Wjl <1284897325@qq.com>
2020-04-04 21:32:20 +0800

    wjl

--- src/com/java/Hello.java
+++ src/com/java/Hello.java
@@ -3,6 +3,6 @@ package com.java;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Test");
-        System.out.println("帅比一个");
+        System.out.println("就是帅比一个");
     }
 }
