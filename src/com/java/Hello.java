79cf70ad97ee45e555b4c36ca8ec759cca46956c
skzd <63005848+skzd@users.noreply.github.com>
2020-04-04 21:37:26 +0800

    skz

--- src/com/java/Hello.java
+++ src/com/java/Hello.java
@@ -3,6 +3,6 @@ package com.java;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Test");
-        System.out.println("skz改动");
+        System.out.println("wjl大帅币");
     }
 }
